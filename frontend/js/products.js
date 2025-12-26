6fd75f0f21e098690f590a4e8876a502bc598dac
Lucas Pereira Dias do Nascimento <lupepdnascimento@gmail.com>
2025-12-26 02:48:35 -0300

    Create products.js

---
+++ frontend/js/products.js
@@ -0,0 +1,26 @@
+export const products = [
+  {
+    id: 1,
+    name: "iPhone 15",
+    price: 7500,
+    category: "celular",
+    image: "",
+    brand: "Apple"
+  },
+  {
+    id: 2,
+    name: "Samsung Galaxy A15",
+    price: 1200,
+    category: "celular",
+    image: "",
+    brand: "Samsung"
+  },
+  {
+    id: 3,
+    name: "iPhone 13",
+    price: 4200,
+    category: "celular",
+    image: "",
+    brand: "Apple"
+  }
+];
