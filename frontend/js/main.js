30450f87e6b62b8a7f6550823a75b92766ac2a56
Lucas Pereira Dias do Nascimento <lupepdnascimento@gmail.com>
2025-12-26 12:35:06 -0300

    adiciona botao de whatsapp por produto

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -10,11 +10,16 @@ function renderProducts() {
     productDiv.classList.add("product");
 
     productDiv.innerHTML = `
-      <h3>${product.name}</h3>
-      <p>Marca: ${product.brand}</p>
-      <p>Preço: R$ ${product.price}</p>
-      <button>Chamar no WhatsApp</button>
-    `;
+  <h3>${product.name}</h3>
+  <p>Marca: ${product.brand}</p>
+  <p>Preço: R$ ${product.price}</p>
+  <a 
+    href="https://wa.me/559935410018?text=Olá! Tenho interesse no produto: ${encodeURIComponent(product.name)}"
+    target="_blank"
+  >
+    <button>Chamar no WhatsApp</button>
+  </a>
+`;
 
     productList.appendChild(productDiv);
   });
